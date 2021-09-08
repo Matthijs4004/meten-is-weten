41ced136cb9f74e629ae579d197de14942ae2f47
Matthijs Raatgever <matthijs4004@gmail.com>
2021-09-08 10:02:17 +0200

    min en max

--- getallen.py
+++ getallen.py
@@ -8,3 +8,10 @@ elif getalA < getalB:
     print("A is het kleinste getal")
 else:
     print("A en B zijn even groot")
+
+if getalA > getalB:
+    print("Het minimum is: " + str(getalB))
+    print("Het maximum is: " + str(getalA))
+elif getalA < getalB: 
+    print("Het minimum is: " + str(getalA))
+    print("Het maximum is: " + str(getalB))
